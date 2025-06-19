3f1091661179d654b276c2f9b287c0bf820a73bb
liara987 <987.liara@gmail.com>
2025-06-19 20:12:24 -0300

    feat: add alias @ to import

--- mb-front-registration/vite.config.js
+++ mb-front-registration/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(__dirname, "src"),
+      },
+    ],
+  },
+});

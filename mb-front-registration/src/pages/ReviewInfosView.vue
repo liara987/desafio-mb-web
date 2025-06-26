551ff6069bc65c01b151de94cc0a71c2b17a696c
liara987 <987.liara@gmail.com>
2025-06-25 22:59:00 -0300

    feat: create review infos view

---
+++ mb-front-registration/src/pages/ReviewInfosView.vue
@@ -0,0 +1,145 @@
+<script setup>
+import { isAllFieldsFilled } from "@/utils/validateFieldFilled";
+import { reactive } from "vue";
+
+const formData = reactive({
+  step: 4,
+  name: "",
+  companyName: "",
+  cpf: "",
+  cnpj: "",
+  birthDay: "",
+  openDate: "",
+  telephone: "",
+  type: "pf",
+});
+
+function sendForm() {
+  console.log(formData);
+}
+</script>
+
+<template>
+  <div class="ReviewInfos">
+    <span class="step">
+      Etapa <span class="hilight-text">{{ formData.step }}</span> de 4
+    </span>
+
+    <h1 class="title">Revise suas informações</h1>
+
+    <form @submit.prevent="sendForm" class="form">
+      <template v-if="formData.type === 'pf'">
+        <label class="name">
+          <span>Nome</span>
+          <input
+            class="input-default"
+            type="text"
+            required
+            v-model="formData.name"
+          />
+        </label>
+
+        <label class="cpf">
+          <span>CPF</span>
+          <input
+            class="input-default"
+            type="text"
+            required
+            v-model="formData.cpf"
+          />
+        </label>
+
+        <label class="birthDay">
+          <span>Data de nascimento</span>
+          <input
+            class="input-default"
+            type="text"
+            required
+            v-model="formData.birthDay"
+          />
+        </label>
+      </template>
+
+      <template v-else>
+        <label class="name">
+          <span>Razão social</span>
+          <input
+            class="input-default"
+            type="text"
+            required
+            v-model="formData.companyName"
+          />
+        </label>
+
+        <label class="cpf">
+          <span>CNPJ</span>
+          <input
+            class="input-default"
+            type="text"
+            required
+            v-model="formData.cnpj"
+          />
+        </label>
+
+        <label class="birthDay">
+          <span>Data de abertura</span>
+          <input
+            class="input-default"
+            type="text"
+            required
+            v-model="formData.openDate"
+          />
+        </label>
+      </template>
+
+      <label class="telephone">
+        <span>Telefone</span>
+        <input
+          class="input-default"
+          type="text"
+          required
+          v-model="formData.telephone"
+        />
+      </label>
+
+      <div class="submit">
+        <button class="btn-secondary" :disabled="!isAllFieldsFilled(formData)">
+          Voltar
+        </button>
+
+        <button
+          type="submit"
+          class="btn-primary"
+          :disabled="!isAllFieldsFilled(formData)"
+        >
+          Continuar
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+
+  label {
+    display: flex;
+    flex-direction: column;
+
+    span {
+      margin: 0.5rem 0; /* 8px */
+    }
+  }
+
+  .submit {
+    display: flex;
+    gap: 1.5rem;
+  }
+
+  button {
+    margin-top: 1rem; /* 16px */
+  }
+}
+</style>

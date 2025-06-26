ba53dd93ace1e6ef78860a793df0b9413a525d16
liara987 <987.liara@gmail.com>
2025-06-25 22:58:11 -0300

    feat: create juridical person view

---
+++ mb-front-registration/src/pages/JuridicalPersonView.vue
@@ -0,0 +1,108 @@
+<script setup>
+import { isAllFieldsFilled } from "@/utils/validateFieldFilled";
+import { reactive } from "vue";
+
+const formData = reactive({
+  step: 2,
+  companyName: "",
+  cnpj: "",
+  openDate: "",
+  telephone: "",
+  type: "",
+});
+
+function sendForm() {
+  console.log(formData);
+}
+</script>
+
+<template>
+  <div class="JuridicalPerson">
+    <span class="step">
+      Etapa <span class="hilight-text">{{ formData.step }}</span> de 4
+    </span>
+
+    <h1 class="title">Pessoa Jurídica</h1>
+
+    <form @submit.prevent="sendForm" class="form">
+      <label class="name">
+        <span>Razão social</span>
+        <input
+          class="input-default"
+          type="text"
+          required
+          v-model="formData.companyName"
+        />
+      </label>
+
+      <label class="cpf">
+        <span>CNPJ</span>
+        <input
+          class="input-default"
+          type="text"
+          required
+          v-model="formData.cnpj"
+        />
+      </label>
+
+      <label class="birthDay">
+        <span>Data de abertura</span>
+        <input
+          class="input-default"
+          type="text"
+          required
+          v-model="formData.openDate"
+        />
+      </label>
+
+      <label class="telephone">
+        <span>Telefone</span>
+        <input
+          class="input-default"
+          type="text"
+          required
+          v-model="formData.telephone"
+        />
+      </label>
+
+      <div class="submit">
+        <button class="btn-secondary" :disabled="!isAllFieldsFilled(formData)">
+          Voltar
+        </button>
+
+        <button
+          type="submit"
+          class="btn-primary"
+          :disabled="!isAllFieldsFilled(formData)"
+        >
+          Continuar
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+
+  label {
+    display: flex;
+    flex-direction: column;
+
+    span {
+      margin: 0.5rem 0; /* 8px */
+    }
+  }
+
+  .submit {
+    display: flex;
+    gap: 1.5rem;
+  }
+
+  button {
+    margin-top: 1rem; /* 16px */
+  }
+}
+</style>

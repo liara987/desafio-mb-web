b2e86cfcb188da6d1727db20851eff0ef66c4690
liara987 <987.liara@gmail.com>
2025-06-25 22:58:34 -0300

    feat: create password view

---
+++ mb-front-registration/src/pages/PasswordView.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { isAllFieldsFilled } from "@/utils/validateFieldFilled";
+import { reactive } from "vue";
+
+const formData = reactive({
+  step: 3,
+  password: "",
+  type: "",
+});
+
+function sendForm() {
+  console.log(formData);
+}
+</script>
+
+<template>
+  <div class="Password">
+    <span class="step">
+      Etapa <span class="hilight-text">{{ formData.step }}</span> de 4
+    </span>
+
+    <h1 class="title">Senha de acesso</h1>
+
+    <form @submit.prevent="sendForm" class="form">
+      <label>
+        <span>Sua senha</span>
+        <input
+          class="input-default"
+          type="text"
+          required
+          v-model="formData.password"
+        />
+      </label>
+
+      <div class="submit">
+        <button class="btn-secondary" :disabled="!isAllFieldsFilled(formData)">
+          Voltar
+        </button>
+
+        <button
+          type="submit"
+          class="btn-primary"
+          :disabled="!isAllFieldsFilled(formData)"
+        >
+          Continuar
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+
+  label {
+    display: flex;
+    flex-direction: column;
+
+    span {
+      margin: 0.5rem 0; /* 8px */
+    }
+  }
+
+  .submit {
+    display: flex;
+    gap: 1.5rem;
+  }
+
+  button {
+    margin-top: 1rem; /* 16px */
+  }
+}
+</style>

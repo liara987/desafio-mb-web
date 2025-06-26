1f4c049c9ec070beec517531297bde7655121d83
liara987 <987.liara@gmail.com>
2025-06-25 21:54:47 -0300

    chore: remove comments and call direct remove not number characters

--- mb-front-registration/src/utils/validateCnpj.js
+++ mb-front-registration/src/utils/validateCnpj.js
@@ -1,9 +1,6 @@
 export function maskCnpj(cnpj) {
-  // Remove tudo que não for número
-  cnpj = cnpj.replace(/\D/g, "");
-
-  // Adiciona os pontos e traço
   cnpj = cnpj
+    .replace(/\D/g, "")
     .replace(/^(\d{2})(\d)/, "$1.$2") // 00.000
     .replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3") // 00.000.000
     .replace(/\.(\d{3})(\d)/, ".$1/$2") // 00.000.000/0000
--- mb-front-registration/src/utils/validateCpf.js
+++ mb-front-registration/src/utils/validateCpf.js
@@ -1,9 +1,6 @@
 export function maskCpf(cpf) {
-  // Remove tudo que não for número
-  cpf = cpf.replace(/\D/g, "");
-
-  // Adiciona os pontos e traço
   cpf = cpf
+    .replace(/\D/g, "")
     .replace(/(\d{3})(\d)/, "$1.$2") // 000.
     .replace(/(\d{3})(\d)/, "$1.$2") // 000.
     .replace(/(\d{3})(\d{1,2})$/, "$1-$2"); // 000-00

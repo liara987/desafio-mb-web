ec83cd08034d83d600b46069a3f9560ba9f94dcc
liara987 <987.liara@gmail.com>
2025-06-28 14:04:58 -0300

    feat: add validation to password

--- mb-front-registration/src/pages/PasswordView.vue
+++ mb-front-registration/src/pages/PasswordView.vue
@@ -1,15 +1,37 @@
 <script setup>
-import { reactive } from "vue";
+import { computed, reactive } from "vue";
 
 import { isAllFieldsFilled } from "@/utils/validateFieldFilled";
+import { isPasswordValid } from "../utils/validatePassword";
+
+const emit = defineEmits(["finish"]);
+
+const errors = reactive({
+  name: false,
+  cpf: false,
+  birthDay: false,
+  telephone: false,
+});
 
 const formData = reactive({
   step: 3,
   password: "",
-  type: "",
 });
 
+const hasErrors = computed(() => {
+  return Object.values(errors).some((err) => err === true);
+});
+
+function handlePasswordBlur() {
+  if (isPasswordValid(formData.password)) {
+    errors.password = false;
+  } else {
+    errors.password = true;
+  }
+}
+
 function sendForm() {
+  emit("finish", formData);
   console.log("sendForm: ", formData);
 }
 </script>
@@ -23,25 +45,37 @@ function sendForm() {
     <h1 class="title">Senha de acesso</h1>
 
     <form @submit.prevent="sendForm" class="form">
-      <label>
+      <label class="password">
         <span>Sua senha</span>
         <input
-          class="input-default"
-          type="text"
           required
-          v-model="formData.password"
+          id="password"
+          type="text"
+          v-model.trim="formData.password"
+          @blur="handlePasswordBlur"
+          @focus="errors.password = false"
+          :class="['input-default', errors.password ? 'input-invalid' : '']"
         />
+        <span v-if="errors.password" class="error-message">
+          Senha deve ter no Mínimo de 8 caracteres
+          <br />
+          No mínimo 1 letra maiúscula
+          <br />
+          No mínimo 1 letra minúscula
+          <br />
+          No mínimo 1 número
+          <br />
+          No mínimo 1 caractere especial (como !@#$%&*)
+        </span>
       </label>
 
       <div class="submit">
-        <button class="btn-secondary" :disabled="!isAllFieldsFilled(formData)">
-          Voltar
-        </button>
+        <button class="btn-secondary">Voltar</button>
 
         <button
           type="submit"
           class="btn-primary"
-          :disabled="!isAllFieldsFilled(formData)"
+          :disabled="!isAllFieldsFilled(formData) || hasErrors"
         >
           Continuar
         </button>

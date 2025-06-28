a6e0651017d24f5258aa68dfbcaa4c2949325821
liara987 <987.liara@gmail.com>
2025-06-28 13:15:21 -0300

    chore: remove console.log and trim

--- mb-front-registration/src/utils/validateFieldFilled.js
+++ mb-front-registration/src/utils/validateFieldFilled.js
@@ -1,7 +1,5 @@
 export function isAllFieldsFilled(formData) {
-  console.log(formData);
-
   return Object.values(formData).every((value) => {
-    return typeof value === "string" ? value.trim() !== "" : true;
+    return typeof value === "string" ? value !== "" : true;
   });
 }

70c2d89eebcf2ce143d6c61f4bef7268261be6f0
liara987 <987.liara@gmail.com>
2025-06-28 13:17:01 -0300

    refact: implements component direct on page

--- mb-front-registration/src/pages/PhysicPersonView.vue
+++ mb-front-registration/src/pages/PhysicPersonView.vue
@@ -1,11 +1,20 @@
 <script setup>
-import { computed, reactive, ref } from "vue";
+import { computed, reactive } from "vue";
 
 import { isAllFieldsFilled } from "@/utils/validateFieldFilled";
+import { isCpfValid } from "../utils/validateCpf";
 
-import InputCpf from "../components/InputCpf.vue";
-import InputDate from "../components/InputDate.vue";
-import InputTelephone from "../components/InputTelephone.vue";
+import { isDateValid } from "../utils/validateDate";
+import { isPhoneValid } from "../utils/validateTelephone";
+
+const emit = defineEmits(["finish"]);
+
+const errors = reactive({
+  name: false,
+  cpf: false,
+  birthDay: false,
+  telephone: false,
+});
 
 const formData = reactive({
   step: 2,
@@ -13,41 +22,47 @@ const formData = reactive({
   cpf: "",
   birthDay: "",
   telephone: "",
-  type: "",
 });
 
-const isBlurred = ref(false);
+const hasErrors = computed(() => {
+  return Object.values(errors).some((err) => err === true);
+});
 
-const isFormValid = computed(() => isAllFieldsFilled(formData));
-
-function handleCpf(cpf) {
-  formData.cpf = cpf;
-  console.log("handleCpf: ", cpf);
-}
-
-function handleTelephone(phone) {
-  formData.telephone = phone;
-  console.log("handleTelephone: ", phone);
-}
-
-function handleDate(date) {
-  formData.birthDay = date;
-  console.log("handleDate: ", date);
-}
-
-function isNameFilled() {
+function handleNameBlur() {
   if (formData.name != "") {
-    // emit("get-name", formData.name);
-    console.log(formData.name);
-
-    isBlurred.value = false;
+    errors.name = false;
   } else {
-    isBlurred.value = true;
+    errors.name = true;
+  }
+}
+
+function handleCpfBlur() {
+  if (isCpfValid(formData.cpf)) {
+    errors.cpf = false;
+  } else {
+    errors.cpf = true;
+  }
+}
+
+function handleBirthDayBlur() {
+  if (isDateValid(formData.birthDay)) {
+    errors.birthDay = false;
+  } else {
+    errors.birthDay = true;
+  }
+}
+
+function handleTelephoneBlur() {
+  if (isPhoneValid(formData.telephone)) {
+    errors.telephone = false;
+  } else {
+    errors.telephone = true;
   }
 }
 
 function sendForm() {
-  console.log(formData);
+  emit("finish", formData);
+  console.log("sendForm: ", formData);
 }
 </script>
 
@@ -67,51 +82,81 @@ function sendForm() {
           id="name"
           type="text"
           v-model.trim="formData.name"
-          @blur="isNameFilled"
-          :class="['input-default', isBlurred ? 'input-invalid' : '']"
+          @blur="handleNameBlur"
+          @focus="errors.name = false"
+          :class="['input-default', errors.name ? 'input-invalid' : '']"
         />
-        <span v-if="isBlurred" class="error-message">
+        <span v-if="errors.name" class="error-message">
           Por favor preencha o Nome corretamente
         </span>
       </label>
 
-      <InputCpf @get-cpf="handleCpf" />
-      <InputDate @get-date="handleDate" />
-      <InputTelephone @get-telephone="handleTelephone" />
+      <label class="cpf">
+        <span>CPF</span>
+        <input
+          required
+          id="cpf"
+          type="text"
+          name="cpf"
+          maxlength="14"
+          v-model.trim="formData.cpf"
+          @blur="handleCpfBlur"
+          @focus="errors.cpf = false"
+          :class="['input-default', errors.cpf ? 'input-invalid' : '']"
+        />
+        <span v-if="errors.cpf" class="error-message">
+          Por favor insira um CPF valido
+        </span>
+      </label>
+
+      <label class="birthDay">
+        <span>Data de Nascimento</span>
+        <input
+          required
+          id="birthDay"
+          type="date"
+          name="birthDay"
+          v-model="formData.birthDay"
+          @blur="handleBirthDayBlur"
+          @focus="errors.birthDay = false"
+          :class="['input-default', errors.birthDay ? 'input-invalid' : '']"
+        />
+        <span v-if="errors.birthDay" class="error-message">
+          Por favor preencha a Data de Nascimento corretamente
+        </span>
+      </label>
+
+      <label class="telephone">
+        <span>Telefone</span>
+        <input
+          required
+          id="telephone"
+          type="tel"
+          maxlength="11"
+          name="telephone"
+          pattern="\d{10,11}"
+          inputmode="numeric"
+          v-model="formData.telephone"
+          @blur="handleTelephoneBlur"
+          @focus="errors.telephone = false"
+          :class="['input-default', errors.telephone ? 'input-invalid' : '']"
+        />
+        <span v-if="errors.telephone" class="error-message">
+          Por favor preencha o Telefone corretamente
+        </span>
+      </label>
 
       <div class="submit">
-        <button class="btn-secondary" :disabled="!isFormValid">Voltar</button>
+        <button class="btn-secondary">Voltar</button>
 
-        <button type="submit" class="btn-primary" :disabled="!isFormValid">
+        <button
+          type="submit"
+          class="btn-primary"
+          :disabled="!isAllFieldsFilled(formData) || hasErrors"
+        >
           Continuar
         </button>
       </div>
     </form>
   </div>
 </template>
-
-<style scoped>
-.form {
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-
-  label {
-    display: flex;
-    flex-direction: column;
-
-    span {
-      margin: 0.5rem 0; /* 8px */
-    }
-  }
-
-  .submit {
-    display: flex;
-    gap: 1.5rem;
-  }
-
-  button {
-    margin-top: 1rem; /* 16px */
-  }
-}
-</style>

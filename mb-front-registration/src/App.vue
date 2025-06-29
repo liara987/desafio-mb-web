408943385f97ea6f00e9dd21e8cbc2716a827bf7
liara987 <987.liara@gmail.com>
2025-06-28 21:24:46 -0300

    feat: render components dynamic

--- mb-front-registration/src/App.vue
+++ mb-front-registration/src/App.vue
@@ -1,16 +1,65 @@
 <script setup>
-import WelcomeView from "./pages/WelcomeView.vue";
-// import ReviewInfos from "@/pages/ReviewInfosView.vue";
-// import PhysicPersonView from "./pages/PhysicPersonView.vue";
-// import JuridicalPersonView from "./pages/JuridicalPersonView.vue";
+import { markRaw, ref } from "vue";
+// markRaw foi utilizado para remover warnings sobre performance
+
+import JuridicalPersonView from "@/pages/JuridicalPersonView.vue";
+import PhysicPersonView from "@/pages/PhysicPersonView.vue";
+import ReviewInfosView from "@/pages/ReviewInfosView.vue";
+import SuccessRegistrationView from "@/pages/SuccessRegistrationView.vue";
+import WelcomeView from "@/pages/WelcomeView.vue";
+import PasswordView from "./pages/PasswordView.vue";
+
+const steps = Object.freeze({
+  WELCOME: {
+    key: "welcome",
+    title: "Seja bem vindo(a)",
+    component: markRaw(WelcomeView),
+  },
+  PHYSIC_PERSON: {
+    key: "pessoa_fisica",
+    title: "Pessoa Física",
+    component: markRaw(PhysicPersonView),
+  },
+  JURIDICAL_PERSON: {
+    key: "pessoa_juridica",
+    title: "Pessoa Jurídica",
+    component: markRaw(JuridicalPersonView),
+  },
+  PASSWORD: {
+    key: "password",
+    title: "Senha de acesso",
+    component: markRaw(PasswordView),
+  },
+  REVIEW_INFOS: {
+    key: "review_infos",
+    title: "Revise suas informações",
+    component: markRaw(ReviewInfosView),
+  },
+  SUCCESS_REGISTRATION: {
+    key: "registration_success",
+    title: "Dados cadastrados com sucesso",
+    component: markRaw(SuccessRegistrationView),
+  },
+});
+
+const formData = ref({});
+const currentStep = ref(steps.WELCOME);
+
+function proximaEtapa(stepName, stepData) {
+  Object.assign(formData.value, stepData);
+
+  currentStep.value = steps[stepName];
+}
 </script>
 
 <template>
   <div class="container">
-    <WelcomeView />
-    <PhysicPersonView />
-    <JuridicalPersonView />
-    <Password />
-    <ReviewInfos />
+    <component
+      :title="currentStep.title"
+      :is="currentStep.component"
+      :step="currentStep"
+      :data="formData"
+      @complete="proximaEtapa"
+    />
   </div>
 </template>

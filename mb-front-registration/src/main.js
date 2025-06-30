2419cce28150e03d5d43091c245321e13ce586ed
liara987 <987.liara@gmail.com>
2025-06-30 20:59:54 -0300

    chore: change fold struct

--- mb-front-registration/src/main.js
+++ mb-front-registration/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import "./style.scss";
+
+import App from "@/App.vue";
+
+import "@/assets/scss/style.scss";
 
 createApp(App).mount("#app");

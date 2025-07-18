1d50607ddecde7f4a6043da1590d300baee3bc38
liara987 <987.liara@gmail.com>
2025-07-17 23:32:54 -0300

    feat: create factory create form data

---
+++ src/utils/formFactory.js
@@ -0,0 +1,73 @@
+// utils/formFactory.js
+
+export function createFormData(type, step = 1, data = {}) {
+  const base = {
+    step: step,
+    email: "",
+    password: "",
+    personType: type,
+  };
+
+  if (type === "pessoa-fisica") {
+    return {
+      ...base,
+      name: "",
+      cpf: "",
+      birthDay: "",
+      telephone: "",
+      ...data,
+    };
+  }
+
+  if (type === "pessoa-juridica") {
+    return {
+      ...base,
+      companyName: "",
+      cnpj: "",
+      openDate: "",
+      telephone: "",
+      ...data,
+    };
+  }
+
+  // fallback genérico, caso o tipo não seja conhecido
+  return {
+    ...base,
+    ...data,
+  };
+}
+
+export function createFormPassword(data = {}) {
+  return {
+    step: 3,
+    password: data.password || "",
+    personType: data.personType || "",
+  };
+}
+
+export function createErrors(typePerson) {
+  return {
+    email: false,
+    telephone: false,
+    password: false,
+    ...(typePerson === "pessoa-fisica"
+      ? {
+          name: false,
+          cpf: false,
+          birthDay: false,
+        }
+      : {
+          companyName: false,
+          cnpj: false,
+          openDate: false,
+        }),
+  };
+}
+
+export function createProps() {
+  return {
+    title: "",
+    step: "",
+    data: {},
+  };
+}

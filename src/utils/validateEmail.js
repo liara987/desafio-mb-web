400262bd01f5e0e0b4b99349a719aeed4cbaf9e8
liara987 <987.liara@gmail.com>
2025-07-17 23:38:52 -0300

    refact: remove code not used

--- src/utils/validateEmail.js
+++
@@ -1,5 +0,0 @@
-export function isEmailValid(email) {
-  //email valido text@texto.text
-  const regex = /^[^\s]+@[^\s]+\.[^\s]+$/;
-  return regex.test(email);
-}
--- src/utils/validateFieldFilled.js
+++
@@ -1,5 +0,0 @@
-export function isAllFieldsFilled(formData) {
-  return Object.values(formData).every((value) => {
-    return typeof value === "string" ? value !== "" : true;
-  });
-}

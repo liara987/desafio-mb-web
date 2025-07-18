d3793ac3271196049b7ebf39442b048eaa2472eb
liara987 <987.liara@gmail.com>
2025-07-18 14:09:59 -0300

    fix: dont override step property

--- src/utils/fillFormData.js
+++ src/utils/fillFormData.js
@@ -1,5 +1,7 @@
 export function fillFormData(formData, payload) {
   for (const key in payload) {
+    if (key === "step") continue;
+
     if (formData.hasOwnProperty(key)) {
       formData[key] = payload[key];
     }
